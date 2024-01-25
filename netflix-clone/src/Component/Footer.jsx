e8e2c91c842cc668991add8f893ef79a77c66c38
gezu2 <gezugudisa@gmail.com>
2024-01-25 05:24:21 +0300

    axios and requests

--- netflix-clone/src/Component/Footer.jsx
+++ netflix-clone/src/Component/Footer.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import "./Footer/Footer.css";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="footer_outer_container">
+      <div className="footer_inner_container">
+        <div className="footer_icons">
+          <FacebookOutlinedIcon />
+          <InstagramIcon />
+          <YouTubeIcon />
+        </div>
+        <div className="footer_data">
+          <div>
+            <ul>
+              <li>Audio Description</li>
+              <li> Investor Relation</li>
+              <li>Legal notice</li>
+            </ul>
+          </div>
+
+          <div>
+            <ul>
+              <li>Help Center</li>
+              <li>Jobs</li>
+              <li>Cookie preference</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Gift Cards</li>
+              <li>Terms of Use</li>
+              <li>Corporate Information</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Media Center</li>
+              <li>Privacy</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+        </div>
+        <div className="service_code">
+          <p>Service Code</p>
+        </div>
+        <div className="copy-write">&copy;1997-2024 Netflix,Ine.</div>
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
--- netflix-clone/src/Pages/Home/Home.jsx
+++ netflix-clone/src/Pages/Home/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Header from '../../Component/Header'
+import Header from '../../Component/Header/Header'
 import Footer from '../../Component/Footer'
 
 function Home() {
   return (
     <>
  <Header/>
-  <Footer/>
+<Footer/>
   </>
   )
 }
